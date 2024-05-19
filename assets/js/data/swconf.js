6c75ddd13c656af233f82f42ca2d110a45153a9c
Jeuk Kim <jeuk20.kim@gmail.com>
2024-05-19 18:37:00 +0900

    charles-proxy: Post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715833408',resources: [
+    cacheName: 'chirpy-1716111352',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
