f9bd6e52331f00db634d435fd7e4e8523abbd535
Jeuk Kim <jeuk20.kim@gmail.com>
2024-05-21 20:51:57 +0900

    favicon.ico added in root directory

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716289531',resources: [
+    cacheName: 'chirpy-1716292160',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
