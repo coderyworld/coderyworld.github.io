f572b00e13988f576123c641577e19b25d6cf13d
Jeuk Kim <jeuk20.kim@gmail.com>
2024-05-21 11:20:30 +0900

    favicon and avatar added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716111352',resources: [
+    cacheName: 'chirpy-1716257994',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
