70234470f1c4f4be8693b5bbf6e4e7c5f739441a
Jeuk Kim <jeuk20.kim@gmail.com>
2024-06-07 19:40:03 +0900

    Moving..

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717754672',resources: [
+    cacheName: 'chirpy-1717756773',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
