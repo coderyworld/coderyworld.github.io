f631b18ab9bdcc22134245881d83c119d797fada
Jeuk Kim <jeuk20.kim@gmail.com>
2024-06-22 18:49:52 +0900

    301 redirection

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719048777',resources: [
+    cacheName: 'chirpy-1719050482',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
